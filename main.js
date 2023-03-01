6674aac8d2f823c0f75702840595d0efe4092d1b
Joseph Ugwu <106489794+ugwuintech@users.noreply.github.com>
2023-03-01 11:51:41 -0800

    added light mode

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
-const tray = document.getElementById("settings-tray");
-const hero = document.getElementById("hero");
-const addBtn = document.getElementById("add-todo");
-const newTask = document.getElementById("new-task-btn");
-const saveBtn = document.getElementById("save-btn");
-
 function openTray() {
   tray.style.width = "150px";
-  lightTheme.style.display = "none";
+  themeSettings.style.display = "none";
 }
 function closeTray() {
   tray.style.width = "50px";
-  lightTheme.style.display = "block";
+  themeSettings.style.display = "block";
 }
 
-function redTheme() {
-  hero.className = "hero red-bg";
-  addBtn.className = "btn task-btn red-bg";
-  newTask.className = "btn task-btn float-end red-bg";
-  saveBtn.className = "btn task-btn red-bg";
+function lightTheme() {
+  const body = document.querySelector("body");
+  body.style.background = "#f5fbff";
+  document.querySelector("nav").className =
+    "navbar navbar-expand-lg navbar-light-mode  sticky-header";
+  tray.style.background = "#2048bb";
   closeTray();
 }
-function greenTheme() {
-  hero.className = "hero green-bg";
-  addBtn.className = "btn task-btn green-bg";
-  newTask.className = "btn task-btn float-end green-bg";
-  saveBtn.className = "btn task-btn green-bg";
-  closeTray();
-}
-function yellowTheme() {
-  hero.className = "hero yellow-bg";
-  addBtn.className = "btn task-btn yellow-bg";
-  newTask.className = "btn task-btn float-end yellow-bg";
-  saveBtn.className = "btn task-btn yellow-bg";
+
+function darkTheme() {
+  const body = document.querySelector("body");
+  body.style.background = "#1b1b1b";
+  document.querySelector("nav").className =
+    "navbar navbar-expand-lg navbar-dark bg-dark sticky-header";
+  tray.style.background = "#212529";
   closeTray();
 }
